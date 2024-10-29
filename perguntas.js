6ef184fa656a8971a2ddfd374ee8c9404828d390
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-10-29 08:17:40 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+criaCartao(
+    'Matemática',
+    '2x+15=25+4x-4x, qual o valor de x?',
+    'x=5.'
+)
+criaCartao(
+    'Português'
+    'O que é um ditongo?'
+    'São palavras que possuem duas vogais juntas.'
+)
+criaCartao( 
+    'Biologia'
+    'Se o homem evoluiu do macaco, porque ainda existem macacos?'
+)
