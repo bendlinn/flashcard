2bf89fb009f6198cfa758c17c583ae5abaf40903
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-10-29 08:22:50 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -6,10 +6,28 @@ criaCartao(
 criaCartao(
     'Português',
     'O que é um ditongo?',
-    'São palavras que possuem duas vogais juntas.',
+    'São palavras que possuem duas vogais juntas.'
 )
 criaCartao(
     'Biologia'
     'Se o homem evoluiu do macaco, porque ainda existem macacos?',
-    'Pois eles possuem um ancestral comum, por isso ainda existem macacos.',
+    'Pois eles possuem um ancestral comum, por isso ainda existem macacos.'
+)
+criaCartao(
+    'Química',
+    'O que é bond line?',
+    'É uma fórmula que representa a cadeia carbônica em ziguezague formando vértices.'
+)
+criaCartao(
+    'Inglês',
+    'Qual a tradução da frase “ I’m going to the mall”?',
+    'Eu estou indo ao shopping.'
+)
+criaCartao(
+    'Matemática 1',
+    'O que é 13° salário?',
+    'É um direito garantido aos trabalhadores brasileiros, consistindo em uma gratificação anual equivalente a 1/12 avos da remuneração por mês de serviço no ano.'
+)
+criaCartao(
+    
 )
