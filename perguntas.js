77145d3ec2bcce1f2202837a13c40d283f07f5f2
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-10-29 08:29:55 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -9,7 +9,7 @@ criaCartao(
     'São palavras que possuem duas vogais juntas.'
 )
 criaCartao(
-    'Biologia'
+    'Biologia',
     'Se o homem evoluiu do macaco, porque ainda existem macacos?',
     'Pois eles possuem um ancestral comum, por isso ainda existem macacos.'
 )
@@ -29,5 +29,32 @@ criaCartao(
     'É um direito garantido aos trabalhadores brasileiros, consistindo em uma gratificação anual equivalente a 1/12 avos da remuneração por mês de serviço no ano.'
 )
 criaCartao(
-    
+    'Fisica',
+    'Para que usamos um resistor?',
+    'Para limitar o fluxo de cargas elétricas por meio da conversão da energia elétrica em energia térmica.'
+)
+criaCartao(
+    'História',
+    'Que ano ocorreu a 2° G.M.?',
+    '1 de setembro de 1939 – 2 de setembro de 1945.'
+)
+criaCartao(
+    'Biologia 1',
+    'Quem foi Gregor Mendel?',
+    'Gregor Johann Mendel foi um monge que realizou estudos sobre hereditariedade usando ervilhas, os quais formaram, mais tarde, a base do que é conhecido como genética.'
+)
+criaCartao(
+    'Sociologia',
+    'O que é relativismo cultural?',
+    'É um conceito e perspectiva que categoriza as culturas como “superiores” ou “inferiores”.'
+)
+criaCartao(
+    'Matemática 2',
+    'Para que serve o github?',
+    'A plataforma ajuda os desenvolvedores a armazenar e administrar o código e faz o registro de mudanças.'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é Tesouro Direto? ',
+    'O Tesouro Direto é uma modalidade de investimento em títulos públicos.'
 )
