9d32e7e200ee6abaf970beb5b80a20fdb2739cba
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-11-05 11:24:25 +0000

    .

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
     cartao.innerHTML =
     <div class="cartao__conteudo">
-                    <h3>Matematica</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p> 2x+15=25+4x-4x, qual o valor de x?</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                        <p>x=5.</p>
+                        <p>${reposta}</p>
                     </div>
                 </div>
 
