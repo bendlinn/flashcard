9c4884532a07cc6d91945dfbe543afa526415933
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-11-05 11:21:35 +0000

    .

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, resposta) { 
-    console.log(categoria, pergunta, resposta);
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML =
+    <div class="cartao__conteudo">
+                    <h3>Matematica</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p> 2x+15=25+4x-4x, qual o valor de x?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>x=5.</p>
+                    </div>
+                </div>
+
+                let container = document.getElementById('container')
+                container.appendChild(cartao)
 }
