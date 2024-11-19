4e3b5d37211cbf95faac3cd5d8ea60487b603f6e
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-11-19 07:43:14 -0300

    .

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ function criaCartao(categoria, pergunta, resposta) {
 `
     let respostaEstaVisivel = false
     function viraCartao(){
-        respostaEstaVisivel = !
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('activ',respostaEstaVisivel)
     }
      let container = document.getElementById('container')
     container.appendChild(cartao)
