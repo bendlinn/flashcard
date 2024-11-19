e108e119cb75cc940619d71946e019a9d7354864
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-11-19 08:00:23 -0300

    active

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let respostaEstaVisivel = false
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('activ',respostaEstaVisivel)
+        cartao.classList.toggle('active',respostaEstaVisivel)
     }
     cartao.addEventListener('click',viraCartao)
      let container = document.getElementById('container')
