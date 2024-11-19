18bb446dea6e9994592b77a1ec86339994f2ee52
bendlinn <140621520+bendlinn@users.noreply.github.com>
2024-11-19 07:32:09 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) { 
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    cartao.innerHTML =
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
@@ -11,7 +11,7 @@ function criaCartao(categoria, pergunta, resposta) {
                         <p>${reposta}</p>
                     </div>
                 </div>
-
+´
                 let container = document.getElementById('container')
                 container.appendChild(cartao)
 }
